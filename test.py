651222e8c6057da922dae166fa50790eb6448012
harry6-25 <harry@LEEdeMacBook-Pro.local>
2021-06-20 14:45:36 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+df = pd.read_csv("Lilith.csv")
+import joblib
+import traceback
+import datetime
+import math
+from PIL import Image
+filename = 'polyreg_model.sav'
+
+loaded_model = joblib.load(filename)
+
+image1 = Image.open('HKSTP.png')
+image2 = Image.open('preface.png')
+st.title("Car Park Vacancies Prediction")
+st.image([image1,image2],width=345)
+district = st.selectbox(
+   'Please select district for prediction',
+   ('Eastern','Kowloon City','Sai Kung','Kwai Tsing','Yau Tsim Mong','Tuen Mun','Wong Tai Sin','Southern','Islands','Yuen Long','Wan Chai',
+'Sham Shui Po','Central & Western','Sha Tin','Tai Po','Kwun Tong','Tsuen Wan','North'))
+
+user_input_time = st.text_input("Please input time you want to predict.")
+try:
+    converted_time =  datetime.datetime.strptime(user_input_time, '%Y-%m-%d %H:%M:%S')
+    day = converted_time.weekday()
+    hour = converted_time.hour
+    minute =converted_time.minute
+    st.write("Predicted Availibility: ",math.floor(loaded_model.predict(np.array([[day, hour, minute]]))[0]))
+except:
+    st.write("Please enter a valid date in yyyy-mm-dd HH:MM:SS")
